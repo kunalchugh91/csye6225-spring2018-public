11860f84abdd16506bfadbf9db28e47167265844
Neel Indap <indap.n@husky.neu.edu>
2018-01-30 16:58:04 -0500

    war creation

--- cloud_project/src/main/java/edu/neu/CloudApplication.java
+++ cloud_project/src/main/java/edu/neu/CloudApplication.java
@@ -2,11 +2,18 @@ package edu.neu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.*;
+import org.springframework.boot.web.support.*;
 
 @SpringBootApplication
-public class CloudApplication {
+public class CloudApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CloudApplication.class);
 	}
 }
