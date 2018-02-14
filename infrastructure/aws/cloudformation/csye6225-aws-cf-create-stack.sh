23d74cdaf4009b724b42f481f8a9b43615d905c6
Neel Indap <indap.n@husky.neu.edu>
2018-02-13 19:05:24 -0500

    Stack termination changes

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -38,7 +38,7 @@ echo "Networking stack creation in progress. Please wait"
 aws cloudformation wait stack-create-complete --stack-name $1-networking
 STACKDETAILS=$(aws cloudformation describe-stacks --stack-name $1-networking --query Stacks[0].StackId --output text)
 echo "Networking stack creation complete"
-echo "Stack id: $STACKDETAILS"
+echo "Networking Stack id: $STACKDETAILS"
 
 echo "Creating application stack"
 
@@ -48,15 +48,14 @@ VPC_ID=$(aws ec2 describe-vpcs --query Vpcs[0].VpcId --output text)
 echo "Fetching domain name from Route 53"
 DOMAIN_NAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
 
-SUBNET_ID_1=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[0].SubnetId" --output text)
-SUBNET_ID_2=$(aws ec2 describe-subnets --filters "Name=vpc-id, Values=$VPC_ID" --query "Subnets[0].SubnetId" --output text)
+SUBNET_ID_1=$(aws cloudformation list-stack-resources --stack-name stack1-networking --query 'StackResourceSummaries[?LogicalResourceId==`PrivateSubnet1`][PhysicalResourceId]' --output text)
+SUBNET_ID_2=$(aws cloudformation list-stack-resources --stack-name stack1-networking --query 'StackResourceSummaries[?LogicalResourceId==`PrivateSubnet2`][PhysicalResourceId]' --output text)
 
 aws cloudformation create-stack --stack-name $1-application --template-body file://./csye6225-cf-application.json --parameters ParameterKey=SUBNETID1,ParameterValue=$SUBNET_ID_1 ParameterKey=SUBNETID2,ParameterValue=$SUBNET_ID_2
 
 aws cloudformation wait stack-create-complete --stack-name $1-application
 STACKDETAILS=$(aws cloudformation describe-stacks --stack-name $1-application --query Stacks[0].StackId --output text)
 echo "Application stack creation complete"
-echo "Stack id: $STACKDETAILS"
-
+echo "Application Stack id: $STACKDETAILS"
 
 exit 0
