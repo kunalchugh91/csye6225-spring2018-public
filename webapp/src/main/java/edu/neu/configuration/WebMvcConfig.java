637a39abef2653057f2474895d7c323d3c31e78a
Neel Indap <indap.n@husky.neu.edu>
2018-02-15 13:10:12 -0500

    CSS fix

--- webapp/src/main/java/edu/neu/configuration/WebMvcConfig.java
+++ webapp/src/main/java/edu/neu/configuration/WebMvcConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
