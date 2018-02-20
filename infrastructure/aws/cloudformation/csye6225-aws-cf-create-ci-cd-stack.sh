109b0f24c52a11f650041473343863fca88a6038
Neel Indap <indap.n@husky.neu.edu>
2018-02-19 23:51:34 -0500

    Code deploy changes

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-ci-cd-stack.sh
@@ -21,7 +21,7 @@ fi
 # Domain name for ARN
 echo "Fetching domain name from Route 53"
 DOMAIN_NAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
-WEBAPP_DOMAIN="web-app."${DOMAIN_NAME%?}
+WEBAPP_DOMAIN="s3."${DOMAIN_NAME%?}
 
 CD_DOMAIN="code-deploy."${DOMAIN_NAME%?}
 
@@ -32,7 +32,7 @@ AWS_REGION="us-east-1a"
 echo "Fetching user's account id"
 ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
-RC=$(aws cloudformation create-stack --stack-name $1-ci-cd --capabilities "CAPABILITY_NAMED_IAM" --template-body file://./csye6225-cf-ci-cd.json --parameters ParameterKey=S3ARN,ParameterValue=arn:aws:s3:::$WEBAPP_DOMAIN ParameterKey=CDAPPNAME,ParameterValue=CodeDeployApplication ParameterKey=CDOMAIN,ParameterValue=$CD_DOMAIN)
+RC=$(aws cloudformation create-stack --stack-name $1-ci-cd --capabilities "CAPABILITY_NAMED_IAM" --template-body file://./csye6225-cf-ci-cd.json --parameters ParameterKey=S3ARN,ParameterValue=arn:aws:s3:::$WEBAPP_DOMAIN ParameterKey=CDAPPNAME,ParameterValue=CSYE6225 ParameterKey=CDOMAIN,ParameterValue=$CD_DOMAIN)
 
 echo "CI stack creation in progress. Please wait"
 aws cloudformation wait stack-create-complete --stack-name $1-ci-cd
